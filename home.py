701f1e0057bdfe928e289a0f3bd4bda54ebca936
hassanazzam129 <107306505+hassanazzam129@users.noreply.github.com>
2022-10-31 16:34:19 +0200

    Add files via upload

---
+++ home.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+
+st.markdown(" <center>  <h1> Sports Wear Store Analysis </h1> </font> </center> </h1> ",
+            unsafe_allow_html=True)
+
+
+
+
+st.image('.\sources\istockphoto-1216658940-612x612.jpg')
